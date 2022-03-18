494cf98885b3ea9c63c9c9f2ab28ac3037881ee1
Kholiavskiy Vladimir <vohol@pop-os.localdomain>
2022-03-18 11:42:29 +0200

    changing tabs paradigma

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const menuBtn = document.querySelector('.header__open-menu')
 const navigationMenu = document.querySelector('.header__navigation')
+const tabsButtons = document.querySelectorAll('.tabs__title')
+const tabsElements = document.querySelectorAll('.tabs__selection')
 
 document.addEventListener('click', openNavigationMenu);
 
@@ -11,3 +13,30 @@ function openNavigationMenu(e) {
     menuBtn.classList.toggle('active-nav')
   }
 }
+
+document.addEventListener('click', tabHandler);
+
+function tabHandler(e) {
+  const target = e.target;
+
+  if (target.classList.contains('tabs__title')) {
+    tabsButtons.forEach(element => {
+      element.classList.remove('tabs__title--active')
+    });
+    tabsElements.forEach(element => {
+      element.classList.remove('tabs__selection--active')
+    });
+    target.classList.add('tabs__title--active')
+    target.nextElementSibling.classList.add('tabs__selection--active')
+  }
+}
+
+NiceSelect.bind(document.getElementById('buy-location-selector'))
+NiceSelect.bind(document.getElementById('buy-property-selector'))
+NiceSelect.bind(document.getElementById('buy-price-selector'))
+NiceSelect.bind(document.getElementById('rent-location-selector'))
+NiceSelect.bind(document.getElementById('rent-property-selector'))
+NiceSelect.bind(document.getElementById('rent-price-selector'))
+NiceSelect.bind(document.getElementById('sell-location-selector'))
+NiceSelect.bind(document.getElementById('sell-property-selector'))
+NiceSelect.bind(document.getElementById('sell-price-selector'))
